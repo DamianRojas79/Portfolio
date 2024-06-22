bfecaa2d22a0a32be7e9a02a6422391240ad475a
Damian Rojas <rojasdamianc@gmail.com>
2024-06-22 15:16:38 -0300

    Se agregan configuraciones para envio de mails

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 app=Flask(__name__)
+
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'aad8bfe78c51f3'
+app.config['MAIL_PASSWORD'] = '565a1f1c6911a5'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail=Mail(app)
+
 
 @app.route ('/')
 def index():
